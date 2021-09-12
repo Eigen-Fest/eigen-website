8665b818a6efe08576cefb0dcffa4423340e38ee
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-09-12 05:41:32 +0530

    file setup

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,8 +1,15 @@
 import React from "react"
 import "./Homepage.scss"
+import BGVDO from "../../assets/vdo/bg.mp4"
+import wallpaper from "../../assets/images/wallpaper.jpg"
 
 const Homepage = () => {
-  return <div>Homepage</div>
+  return (
+    <div>
+      {/* <video src={BGVDO} autoPlay muted loop width="1536px"></video> */}
+      <img src={wallpaper} alt="" width="1536px" />
+    </div>
+  )
 }
 
 export default Homepage
